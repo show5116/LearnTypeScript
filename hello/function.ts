643a22a0a14151cc8edca7f8c6f5fb78fe9520f0
youyeongjin <show5116@naver.com>
2022-04-26 13:06:39 +0900

    typescript function

---
+++ hello/function.ts
@@ -0,0 +1,17 @@
+// 함수는 매개변수와 반환값의 타입을 지정하면서 함수를 지정한다.
+function add(numA : number, numB : number): number{
+    return numA + numB;
+}
+
+// void 타입은 값을 반환하지 않는 타입이다.
+function printScreen(any : any): void {
+    console.log(any);
+}
+
+let sum : number = add(1,2);
+printScreen(sum);
+
+// never 타입은 오류를 출력하거나, 리턴 값을 내보내지 않는다.
+function invalid(message : string): never {
+    throw new Error(message);
+}
